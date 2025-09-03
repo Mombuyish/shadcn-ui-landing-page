a27bf9aa66ed95c0128e5d9af77cea699af573ee
Mombuyish <mombuartworks@gmail.com>
2025-09-03 15:07:25 +0800

    ev4

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,22 +9,20 @@ const Hero = () => {
       <div className="max-w-(--breakpoint-xl) w-full flex flex-col lg:flex-row mx-auto items-center justify-between gap-y-14 gap-x-10 px-6 py-12 lg:py-0">
         <div className="max-w-xl">
           <Badge className="rounded-full py-1 border-none">
-            Just released v1.0.0
+            Coming Soon
           </Badge>
           <h1 className="mt-6 max-w-[20ch] text-3xl xs:text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold leading-[1.2]! tracking-tight">
-            Customized Shadcn UI Blocks & Components
+            RecordWise
           </h1>
           <p className="mt-6 max-w-[60ch] xs:text-lg">
-            Explore a collection of Shadcn UI blocks and components, ready to
-            preview and copy. Streamline your development workflow with
-            easy-to-implement examples.
+            The smartest AI meeting & note assistant built for knowledge workers, professional salespeople, podcasters, and researchers.
           </p>
           <div className="mt-12 flex flex-col sm:flex-row items-center gap-4">
             <Button
               size="lg"
               className="w-full sm:w-auto rounded-full text-base"
             >
-              Get Started <ArrowUpRight className="h-5! w-5!" />
+              Waitlist <ArrowUpRight className="h-5! w-5!" />
             </Button>
             <Button
               variant="outline"
